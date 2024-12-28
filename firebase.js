68ddaf56e449929abc4798f362fed930c0ae496c
dinata78 <stevendinata78@gmail.com>
2024-12-28 16:08:39 +0700

    Set up Firebase integration

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhxRqthAHC7jOAjA18b1bSVXTfvqxRB9w",
+  authDomain: "cyber-chat-78.firebaseapp.com",
+  projectId: "cyber-chat-78",
+  storageBucket: "cyber-chat-78.firebasestorage.app",
+  messagingSenderId: "1092074312984",
+  appId: "1:1092074312984:web:c6373958f8157757f6ba6d"
+};
+
+const app = initializeApp(firebaseConfig);
