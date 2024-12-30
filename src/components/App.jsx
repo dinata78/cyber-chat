7369361eed4327a293cd324b3f06401d85e123f1
dinata78 <stevendinata78@gmail.com>
2024-12-30 15:36:48 +0700

    Set up react-router-dom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,15 @@
 import { Auth } from "./Auth";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Auth />,
+  },
+]);
 
 export function App() {
   return (
-    <Auth />
+    <RouterProvider router={router}/>
   )
 }
