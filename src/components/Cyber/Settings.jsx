0dc6949cfd4ef1c606d796bd1f81f26feaf7d9b2
dinata78 <stevendinata78@gmail.com>
2025-01-06 20:51:50 +0700

    Add Settings component's elements

--- src/components/Cyber/Settings.jsx
+++ src/components/Cyber/Settings.jsx
@@ -1,4 +1,27 @@
+import { InputTypeToggle } from "./InputTypeToggle";
 
 export function Settings() {
-  return <span>This is Settings.</span>
+  const onContrastToggle = () => {
+    console.log("Contrast Mode Toggled.");
+  }
+
+  return (
+    <div id="cyber-settings-container">
+      <h1>Settings</h1>
+      <div id="cyber-settings-features">
+        <div className="settings-feature">
+          <span>Font Size</span>
+          <select>
+            <option>Small</option>
+            <option>Normal</option>
+            <option>Large</option>
+          </select>
+        </div>
+        <div className="settings-feature">
+          <span>Contrast Mode</span>
+          <InputTypeToggle onClick={onContrastToggle}/>
+        </div>
+      </div>
+    </div>
+  )
 }
