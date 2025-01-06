91cde5404d678885d6be14ec554f497fd84f2216
dinata78 <stevendinata78@gmail.com>
2025-01-06 17:15:26 +0700

    Add Account component's elements

--- src/components/Cyber/Account.jsx
+++ src/components/Cyber/Account.jsx
@@ -1,6 +1,10 @@
 import { signOut } from "firebase/auth";
 import { auth } from "../../../firebase";
 import { useNavigate } from "react-router-dom";
+import { EditIconSVG } from "../svg/EditIconSVG";
+import { NameIconSVG } from "../svg/NameIconSVG";
+import { BioIconSVG } from "../svg/BioIconSVG";
+import { EmailIconSVG } from "../svg/EmailIconSVG";
 
 export function Account() {
   const navigate = useNavigate();
@@ -16,9 +20,60 @@ export function Account() {
   }
   
   return (
-    <div>
-      <span>This is Account.</span>
-      <button onClick={logOut}>Log Out</button>
+    <div id="cyber-account-container">
+      <div id="cyber-account-pfp">
+        <div id="account-pfp-container">
+          <img id="account-pfp" src="/empty-pfp.webp" />
+          <div id="pfp-edit-icon-container">
+            <EditIconSVG className="pfp-edit-icon" />
+          </div>
+        </div>
+      </div>
+      <div id="cyber-account-features">
+        <div id="account-feature-container">
+          <div className="account-feature">
+            <div className="account-info-icon">
+              <NameIconSVG />
+            </div>
+            <div className="account-info">
+              <label>Name</label>
+              <span>Anonymous</span>
+            </div>
+            <div className="account-info-edit-icon">
+              <EditIconSVG className="info-edit-icon" />
+            </div>
+          </div>
+          <div className="account-feature">
+            <div className="account-info-icon">
+              <BioIconSVG />
+            </div>
+            <div className="account-info">
+              <label>Bio</label>
+              <span>Hello world! How are you guys doing?</span>
+            </div>
+            <EditIconSVG className="info-edit-icon" />
+          </div>
+          <div className="account-feature">
+            <div className="account-info-icon">
+              <EmailIconSVG />
+            </div>
+            <div className="account-info">
+              <label>Email</label>
+              <span>myemail@mail.com</span>
+            </div>
+          </div>
+
+          <div className="account-feature">
+            <button>Reset Password</button>
+          </div>
+          <div className="account-feature">
+            <button onClick={logOut}>Log Out</button>
+          </div>
+          <div className="account-feature">
+            <button>Delete Account</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
