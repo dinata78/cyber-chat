9b6c977294bb0adf3be047233360f58d7d7b4f78
dinata78 <stevendinata78@gmail.com>
2025-01-27 23:34:46 +0700

    Modify Pending Card to display both sent and received friend request

--- src/components/Cyber/Friends/FriendsPending.jsx
+++ src/components/Cyber/Friends/FriendsPending.jsx
@@ -1,12 +1,40 @@
+import { PendingCard } from "./PendingCard"
 
 export function FriendsPending({ friendRequestSentList, friendRequestReceivedList }) {
 
   return (
     <div id="friends-pending">
-      <h1>Pending - {friendRequestSentList.length + friendRequestReceivedList.length}</h1>
-      <div className="friend-cards">
-        
+
+      <div>
+        <h1>
+          Pending Request 
+          - {friendRequestSentList.length 
+              + friendRequestReceivedList.length}
+        </h1>
+        <div className="pending-cards overflow-y-support">
+          {
+            friendRequestSentList.map((requestSentUid) => {
+              return (
+                <PendingCard
+                  type="sent"
+                  uid={requestSentUid}
+                />
+              )
+            })
+          }
+          {
+            friendRequestReceivedList.map((requestReceivedUid) => {
+              return (
+                <PendingCard
+                  type="received"
+                  uid={requestReceivedUid}
+                />
+              )
+            })
+          }
+        </div>
       </div>
+
     </div>
   )
 }
--- src/components/Cyber/Friends/PendingCard.jsx
+++ src/components/Cyber/Friends/PendingCard.jsx
@@ -1,8 +1,54 @@
+import { useEffect, useState } from "react"
+import { fetchDataFromUid } from "../../../utils";
 
-export function PendingCard() {
+export function PendingCard({ type, uid }) {
+  const [name, setName] = useState("...");
+  const [username, setUsername] = useState("...");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchDataFromUid(uid);
+      setName(data.name);
+      setUsername(data.username);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="pending-card">
-      
+      {
+        type === "received" ?
+          <>
+            <span>
+              <b style={{color:"#aaddff"}}>{name} (@{username})</b> sent you a friend request!
+            </span>
+            <div className="pending-card-buttons">
+              <button
+                style={{color: "#00ff62"}}
+              >
+                ACCEPT
+              </button>
+              <button
+                style={{color: "red"}}
+              >
+                REJECT
+              </button>
+            </div>
+          </>
+        :
+          <>
+            <span>
+              You sent <b style={{color: "#aaddff"}}>{name} (@{username})</b> a friend request!
+            </span>
+            <div className="pending-card-buttons">
+              <button
+                style={{color: "red"}}
+              >
+                CANCEL
+              </button>
+            </div>
+          </>
+      }
     </div>
   )
 }
