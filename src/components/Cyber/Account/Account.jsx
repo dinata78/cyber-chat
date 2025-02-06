befdcd15dc61197bb31f87ca7e41e826ecae49d4
dinata78 <stevendinata78@gmail.com>
2025-02-06 09:14:24 +0700

    Clear Account page before rebuilding it

--- src/components/Cyber/Account/Account.jsx
+++ src/components/Cyber/Account/Account.jsx
@@ -17,7 +17,6 @@ export function Account({ ownData }) {
       const dbRef = ref(realtimeDb, `users/${auth.currentUser.uid}`);
 
       await signOut(auth);
-
       await update(dbRef, { isOnline: false }); 
 
       navigate("/");  
@@ -29,91 +28,7 @@ export function Account({ ownData }) {
   
   return (
     <div id="cyber-account">
-      <div id="cyber-account-pfp">
-        <div id="account-pfp">
-          <img src="/empty-pfp.webp" />
-          <div id="account-pfp-edit">
-            <EditSVG />
-          </div>
-        </div>
-      </div>
-      <hr id="account-pfp-features-border"/>
-      <div id="cyber-account-features">
-        <div id="account-features-container" className="overflow-y-support">
-          <div className="account-feature">
-            <div className="info-icon">
-              <AtSVG />
-            </div>
-            <div className="account-info">
-              <label>Username</label>
-              <span>{ownData.username ? ownData.username : "(Not Set)"}</span>
-            </div>
-            <div className="edit-icon">
-              <EditSVG />
-            </div>
-          </div>
-          <hr />
-          <div className="account-feature">
-            <div className="info-icon">
-              <NameSVG />
-            </div>
-            <div className="account-info">
-              <label>Display Name</label>
-              <span>{ownData.name}</span>
-            </div>
-            <div className="edit-icon">
-              <EditSVG />
-            </div>
-          </div>
-          <hr />
-          <div className="account-feature">
-            <div className="info-icon">
-              <TitleSVG />
-            </div>
-            <div className="account-info">
-              <label>Title</label>
-              <span>{ownData.title}</span>
-            </div>
-            <div className="edit-icon">
-              <EditSVG />
-            </div>
-          </div>
-          <hr />
-          <div className="account-feature">
-            <div className="info-icon">
-              <BioSVG />
-            </div>
-            <div className="account-info">
-              <label>Bio</label>
-              <span>{ownData.bio}</span>
-            </div>
-            <div className="edit-icon">
-              <EditSVG />
-            </div>
-          </div>
-          <hr />
-          <div className="account-feature">
-            <div className="info-icon">
-              <EmailSVG />
-            </div>
-            <div className="account-info">
-              <label>Email</label>
-              <span>{ownData.email}</span>
-            </div>
-          </div>
-          <hr />
-          <div className="account-feature">
-            <button>Reset Password</button>
-          </div>
-          <div className="account-feature">
-            <button onClick={logOut}>Log Out</button>
-          </div>
-          <div className="account-feature">
-            <button>Delete Account</button>
-          </div>
-          <hr />
-        </div>
-      </div>
+
     </div>
   )
 }
